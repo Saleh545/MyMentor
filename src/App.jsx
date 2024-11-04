8d750f27373e47f245d6bb72c6ff9a64a743b29d
Saleh545 <syusifzade545@gmail.com>
2024-11-04 18:42:03 +0400

    first page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import icon from "../public/Images/MyMentor.jpg";
+import Header from "./Components/Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+     <header>
+      <div className="container">
+        
+      </div>
+     </header>
+    </div>
+  )
+}
+
+export default Header
